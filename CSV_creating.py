bfa70c3cfaae2893869785e329aa19ec251c4045
Konstantin <legenzov-k@ya.ru>
2022-12-23 16:30:12 +0300

    Finished

--- CSV_creating.py
+++ CSV_creating.py
@@ -1,4 +1,6 @@
+# Создадим функцию которая будет создавать файл с записями
+
 def creating ():
     file = 'Phonebook.csv'
     with open (file, 'w', encoding = 'utf-8') as data:
-        data.write(f'Фамилия;Имя;Номер телефона;Описание\n')
+        data.write(f'Фамилия|Имя|Номер телефона|Описание\n')
--- File_writing.py
+++ File_writing.py
@@ -4,9 +4,10 @@ info = gi()
 def writing_scv ():
     file = 'Phonebook.csv'
     with open (file, 'a', encoding = 'utf-8') as data:
-        data.write(f'{info[0]};{info[1]};{info[2]};{info[3]}\n')
+        data.write(f'{info[0]}|{info[1]}|{info[2]}|{info[3]}\n')
 
 def writing_txt ():
     file = 'Phonebook.txt'
     with open (file, 'a', encoding = 'utf-8') as data:
-        data.write(f'Фамилия: {info[0]}\n\nИмя: {info[1]}\n\nНомер телефона: {info[2]}\n\nОписание: {info[3]}\n\n\n')
+        data.write(f'Фамилия: {info[0]}\nИмя: {info[1]}\nНомер телефона: {info[2]}\n\n Описание: {info[3]}\n\n')
+                                                                
